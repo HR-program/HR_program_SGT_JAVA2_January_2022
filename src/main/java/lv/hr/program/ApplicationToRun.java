d01fe7858d08b7b6fe6fabc3b060a42ced3e5153
Oxana <oksanabelajeva@inbox.lv>
2022-01-20 14:41:54 +0200

    Upload files function added.

--- src/main/java/lv/hr/program/ApplicationToRun.java
+++ src/main/java/lv/hr/program/ApplicationToRun.java
@@ -1,12 +1,25 @@
 package lv.hr.program;
 
+import lv.hr.program.services.FilesStorageService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.Resource;
+
 @SpringBootApplication
-public class ApplicationToRun {
+public class ApplicationToRun implements CommandLineRunner {
+
+    @Resource
+    FilesStorageService storageService;
 
         public static void main(String[] args) {
         SpringApplication.run(ApplicationToRun.class, args);
+    }
+
+    @Override
+    public void run(String... arg) throws Exception {
+        storageService.deleteAll();
+        storageService.init();
     }
 }
